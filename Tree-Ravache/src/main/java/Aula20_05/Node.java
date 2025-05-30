7cc69483cebb299faded63eec70f533b4a9c946e
Fernanda Tozzi <157314646+FernandaTozzi@users.noreply.github.com>
2025-05-29 21:03:31 -0300

    AVL - Update Node.java

--- Tree-Ravache/src/main/java/Aula20_05/Node.java
+++ Tree-Ravache/src/main/java/Aula20_05/Node.java
@@ -4,10 +4,12 @@ public class Node {
     public String valor;
     public Node esquerda;
     public Node direita;
+    public int altura;
 
     public Node(String valor) {
         this.valor = valor;
         this.esquerda = null;
         this.direita = null;
+        this.altura = 1;
     }
 }
